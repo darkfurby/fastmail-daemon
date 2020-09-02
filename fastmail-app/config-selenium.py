1500881c491cc7b1fb2e2acf174fcfa28258b824
Oliver Mackiewicz <oliver.mackiewicz@gmail.com>
2020-09-02 21:04:10 +0200

    driver, logging feature

---
+++ fastmail-app/config-selenium.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import sys
+
+
+login = sys.argv[1] 
+password = sys.argv[2]
+
+
+def driver_return(url):
+    #config
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome('../driver/chromedriver', options=options)
+    # driver = webdriver.Chrome('../driver/chromedriver')
+    driver.get(url)
+    return driver
+
+def login_to_website(driver):
+    #login to website
+    login_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "v16-input")))
+    password_element = driver.find_element_by_id("v17-input")
+    login_element.send_keys(login)
+    password_element.send_keys(password)
+    button_confirm_credentials = driver.find_element_by_class_name("v-Button--constructive")
+    button_confirm_credentials.click()
+
+def get_emails(driver):
+    #get emails
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "v35")))
+    email_container_element = driver.find_element_by_id('v35')
+    emails_array = email_container_element.find_elements_by_tag_name('a')
+
+    for email in emails_array:
+        email_content_element = email.find_elements_by_tag_name('div')
+        for email_part in email_content_element:
+            if len( email_part.get_attribute('title') ) > 0:
+                print(email_part.get_attribute('title'))
+
+def main():
+    driver = driver_return('https://www.fastmail.com/login/')
+    login_to_website(driver)
+    get_emails(driver)
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+
