d3d2b0991070723ceca21f3370fa5662b696e3c0
Oliver Mackiewicz <oliver.mackiewicz@gmail.com>
2020-09-04 16:53:20 +0200

    resolving issues

--- fastmail-app/config-selenium.py
+++ fastmail-app/config-selenium.py
@@ -22,6 +22,7 @@ def driver_return(url):
     #config
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
+    options.add_argument('--window-size=1920,1080')
     driver = webdriver.Chrome('../driver/chromedriver', options=options)
     # driver = webdriver.Chrome('../driver/chromedriver')
     driver.get(url)
@@ -44,12 +45,15 @@ def get_emails_and_content(driver):
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME , "app-list")))
     email_container_element = driver.find_element_by_class_name('app-list')
     emails_array = email_container_element.find_elements_by_tag_name('a')
+    # print("liczba maili ktore zgarnal" + str( len(emails_array) ))
 
     for email in emails_array:
+        # print( email.get_attribute('id') )
         email.click()
         email_object = Email()
         
         div_email_elements = email.find_elements_by_tag_name('div')
+        
         for div in div_email_elements:
 
             if div.get_attribute('class') == "v-MailboxItem-subject u-ellipsis":
@@ -61,6 +65,7 @@ def get_emails_and_content(driver):
             inside_div = div.find_elements_by_tag_name('span')
             for span in inside_div:
                 if span.get_attribute('class') == "v-MailboxItem-name":
+                    get_sender = ''
                     attempts = 0
                     while(attempts < 3):
                         try:
@@ -70,8 +75,9 @@ def get_emails_and_content(driver):
                             print("nie widzi elementu")
 
                     email_object.sender = get_sender[0].get_attribute('title')
+                    
 
-        email.click()
+        # email.click()
         WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH , "//*[contains(@class, 'u-article')]")))
         content = driver.find_element_by_xpath("//*[contains(@class, 'u-article')]")
         cont = content.get_attribute('innerHTML')
@@ -83,7 +89,10 @@ def get_emails_and_content(driver):
 def sele_exec():
     driver = driver_return('https://www.fastmail.com/login/')
     login_to_website(driver)
+    dawajListe = get_emails_and_content(driver)
+    for i in dawajListe:
+        print( i.sender + " " +  i.subject + " " + i.time + " " + i.content)
     return get_emails_and_content(driver)
-
+    driver.close()
 
 sele_exec()
